a2d67d8cd894a0dca5d95e3bff576e56cc379af5
João Pedro <jopmoura49@gmail.com>
2025-07-21 11:30:36 -0300

    remover chupeta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4">
-      <h2 className="text-xl sm:text-2xl font-bold text-center">
-        Bem vindo, chupeta!
-      </h2>
+      <h2 className="text-xl sm:text-2xl font-bold text-center">Bem vindo!</h2>
     </div>
   );
 }
